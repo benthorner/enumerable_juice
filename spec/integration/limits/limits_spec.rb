34c399c58a46c9b4e302c04ade3beefcc93bd48d
Ben Thorner <ben.thorner@bt.com>
2014-12-21 00:27:50 +0000

    Include error messages in limit integration spec

--- spec/integration/limits/limits_spec.rb
+++ spec/integration/limits/limits_spec.rb
@@ -1,23 +1,36 @@
 require './lib/limits/enumerable_patch'
+require './lib/limits/validation_error'
 
 feature 'limits', integration: true do
   scenario 'limit to a single item' do
     expect(['foo'].single).to eq 'foo'
-    expect{ [].single }.to raise_error
+
+    expect{ [].single }.to raise_error(
+      ValidationError, 'Invalid number of items (0 != 1)'
+    )
   end
 
   scenario 'limit to exactly 5 items' do
     expect([1,2,3,4,5].limit.exactly(5)).to eq [1,2,3,4,5]
-    expect([1,2,3].limit.exactly(5)).to raise_error
+
+    expect{ [1,2,3].limit.exactly(5) }.to raise_error(
+      ValidationError, 'Invalid number of items (3 != 5)'
+    )
   end
 
   scenario 'limit to less than 2 items' do
     expect([1,2].limit.at_most(2)).to eq [1,2]
-    expect([1,2,3,4].limit.at_most(2)).to raise_error
+
+    expect{ [1,2,3,4].limit.at_most(2) }.to raise_error(
+      ValidationError, 'Invalid number of items (4 > 2)'
+    )
   end
 
-  scenario 'limit to more than 0 items' do
-    expect([1,2,3].limit.at_least(0)).to eq [1,2,3]
-    expect([].limit.at_least(0)).to raise_error
+  scenario 'limit to more than 1 items' do
+    expect([1,2,3].limit.at_least(1)).to eq [1,2,3]
+
+    expect{ [].limit.at_least(1) }.to raise_error(
+      ValidationError, 'Invalid number of items (0 < 1)'
+    )
   end
 end
